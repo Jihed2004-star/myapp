0d291a536bb99be355a40b8e17359e44520eb23a
Jihed <gharbijihed2004@gmail.com>
2026-07-31 11:28:13 +0100

    another changes

--- server/MyApp.Data/obj/Debug/net10.0/MyApp.Data.AssemblyInfo.cs
+++ server/MyApp.Data/obj/Debug/net10.0/MyApp.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bc5bae4d6e55db817f6f97c55eaa805c16605a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd9042e16452efdc0592207d756f5dae4f3ffb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
